0923b8908058eee80e7142e26245a128530ab63c
Jeff Chiu <jwchiu82@gmail.com>
2021-06-15 19:34:53 -0700

    practice basic functionality of dictionary usage

---
+++ day-9-practice.py
@@ -0,0 +1,18 @@
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that can be reused",
+}
+#retrieving info
+print(programming_dictionary["Bug"])
+
+#adding new items
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+print(programming_dictionary)
+
+#create empty dictionary
+empty_dictionary = {}
+
+#wipe a existing dictionary
+# programming_dictionary = {}
+
+#looping through dictionary
